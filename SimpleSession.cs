c612bb4ddea336c5a0f1911f8083772c401de9f1
drooniq <drooniq@icyheart.org>
2024-01-28 19:20:12 +0100

    Add project files.

---
+++ SimpleSession.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSession
+{
+    public class SimpleSession
+    {
+        /*
+
+                 in future, add IPadress?
+                 add Thread
+                 add Timers
+
+        */
+        private string _sessionId;
+
+        public string SessionId
+        {
+            get => _sessionId;
+            set => _sessionId = value;
+        }
+
+        public string? Username { get; set; }
+
+        public SimpleSession(string sessionId, string? userName)
+        {
+            _sessionId = sessionId;
+            Username = userName;
+        }
+    }
+}
---
+++ SimpleSessionHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace SimpleSession
+{
+    public static class SimpleSessionHandler
+    {
+        public static List<SimpleSession> ActiveSessions { get; } = new List<SimpleSession>();
+
+        public static void AddSession(SimpleSession session)
+        {
+            if (ActiveSessions.Contains(session))
+                return;
+
+            ActiveSessions.Add(session);
+        }
+
+        public static void RemoveSession(string sessionId)
+        {
+            var foundSession = ActiveSessions.Find(s => s.SessionId == sessionId);
+            if (foundSession == null)
+                return;
+            ActiveSessions.Remove(foundSession);
+        }
+
+        public static SimpleSession? GetSessionById(string sessionId)
+        {
+            return ActiveSessions.Find(s => s.SessionId == sessionId);
+        }
+
+        public static void Clear()
+        {
+            ActiveSessions.Clear();
+        }
+    }
+
+
+}
